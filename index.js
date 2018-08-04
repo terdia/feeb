a6d73efc7a090541bf8b104940a3aa5829c80fae
Ogbemudia Terry Osayawe <terrymarcy2000@yahoo.com>
2018-08-04 16:33:32 +0200

    adding google oauth

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
+require("./services/passport");
 
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send({ hey: 'Hello word fron node app. heroku'})
-});
+require("./routes/auth")(app);
 
+app.get("/", (request, response) => {
+  response.send({ hey: "Hello word fron node app. heroku" });
+});
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
-
